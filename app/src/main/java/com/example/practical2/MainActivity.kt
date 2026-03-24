2f64309fae2baf6aa7481525072e858b51669539
Nilakshi Samarasekara <nilakshisamarasekara0@gmail.com>
2026-03-24 15:18:41 +0530

    Practical 3

--- app/src/main/java/com/example/practical2/MainActivity.kt
+++ app/src/main/java/com/example/practical2/MainActivity.kt
@@ -3,71 +3,67 @@ package com.example.practical2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.practical2.ui.theme.Practical2Theme
 import androidx.compose.ui.unit.sp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Practical2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize()
-                    ) {
-                        Greeting(name = "Android")
-                        Hello()
-                    }
-                }
-            }
+            ProfileScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ProfileScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ProfileTitle()
+        ProfileImage()
+        ProfileDescription()
+    }
+}
+
+@Composable
+fun ProfileTitle() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier.padding(8.dp)
+        text = "Nilakshie S",
+        modifier = Modifier.padding(16.dp),
+        fontSize = 22.sp
     )
 }
 
 @Composable
-fun Hello() {
-    Column {
-        Text(
-            text = "Hello Student",
-            modifier = Modifier.padding(8.dp)
-        )
-
-        Text(
-            text = "Welcome to Android",
-            modifier = Modifier
-                .background(Color.Green)
-                .padding(8.dp),
-            fontSize = 40.sp
-        )
-    }
+fun ProfileImage() {
+    Icon(
+        imageVector = Icons.Default.AccountCircle,
+        contentDescription = "Profile Image",
+        modifier = Modifier
+            .padding(16.dp)
+            .size(120.dp)
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun PreviewAll() {
-    Practical2Theme {
-        Column {
-            Greeting("Android")
-            Hello()
-        }
-    }
+fun ProfileDescription() {
+    Text(
+        text = "Nilakshi is a professional Mobile app developer",
+        modifier = Modifier
+            .padding(16.dp)
+
+    )
 }
